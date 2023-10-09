204a6ccd387f36df1438643028a45332574aa4e9
Snehil sagar jain <42835593+snehilsagarjain@users.noreply.github.com>
2023-10-10 00:48:03 +0530

    Add files via upload

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Auth0Features } from "../components/auth0-features";
+//import { Auth0Features } from "../components/auth0-features";
 import { HeroBanner } from "../components/hero-banner";
 import { PageLayout } from "../components/page-layout";
-
+//import { Slideshow } from "../components/tech-slideshow";
 export const HomePage = () => (
   <PageLayout>
     <HeroBanner />
-    <Auth0Features />
+    {/*<Slideshow/>*/}
+    {/*<Auth0Features />*/}
   </PageLayout>
 );
