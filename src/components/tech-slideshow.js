d8fa7086472189ff25f3c73bcff01a5552335cfd
Snehil sagar jain <42835593+snehilsagarjain@users.noreply.github.com>
2023-10-09 23:34:49 +0530

    Add files via upload

---
+++ src/components/tech-slideshow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./tech-slideshow.css";
+export const Slideshow = () => {
+  return (
+    <div className="tech-slideshow">
+      <div className="mover-1"></div>
+      <div className="mover-2"></div>
+    </div>
+  );
+};
